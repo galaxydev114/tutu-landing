277bbabd81075792ba22ed3da9f414de5a4be6d4
galaxyme-github <64599248+galaxyme-github@users.noreply.github.com>
2021-12-15 09:14:25 +0600

    fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import NextApp from './NextApp';
-import * as serviceWorker from './serviceWorker';
+import register, { unregister } from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 
 // Wrap the rendering in a function
@@ -18,7 +18,7 @@ const render = Component => {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
